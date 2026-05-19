e28d3a743dbc418042a7f1db7131cacbf4ef8e45
Bettina Santana de Meirelles <103582720+BettinaSM@users.noreply.github.com>
2026-05-19 02:27:49 -0300

    Create settings.py

---
+++ configs/settings.py
@@ -0,0 +1,13 @@
+APP_NAME = "Unified Enterprise Security Operations Platform"
+
+SUPPORTED_ENVIRONMENTS = [
+    "Linux",
+    "AIX",
+    "Windows",
+    "AWS",
+    "Azure",
+    "GCP",
+    "OCI",
+    "IBM Cloud",
+    "Kubernetes"
+]
