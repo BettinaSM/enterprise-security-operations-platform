c82432684e93c52bdee4afa05957ba48a5fc8437
Bettina Santana de Meirelles <103582720+BettinaSM@users.noreply.github.com>
2026-05-20 02:28:09 -0300

    Update main.py

--- dashboard-app/api/main.py
+++ dashboard-app/api/main.py
@@ -17,6 +17,19 @@ def root():
     }
 
 # ---------------------------
+# TOKEN
+# ---------------------------
+
+from parsers.jwt_engine import (
+    generate_token,
+    validate_token
+)
+
+from parsers.auth_engine import (
+    authenticate
+)
+
+# ---------------------------
 # IOC FEED
 # ---------------------------
 
@@ -82,4 +95,68 @@ def threat_feed():
                 "indicator": "185.220.101.1"
             }
         ]
+    }
+
+# ---------------------------
+# LOGIN
+# ---------------------------
+
+@app.post("/login")
+
+def login(
+    credentials: dict
+):
+
+    username = credentials.get(
+        "username"
+    )
+
+    password = credentials.get(
+        "password"
+    )
+
+    role = authenticate(
+        username,
+        password
+    )
+
+    if not role:
+
+        return {
+            "error": "Invalid credentials"
+        }
+
+    token = generate_token(
+        username,
+        role
+    )
+
+    return {
+        "token": token,
+        "role": role
+    }
+
+# ---------------------------
+# PROTECTED ENDPOINT
+# ---------------------------
+
+@app.get("/protected")
+
+def protected(
+    token: str
+):
+
+    payload = validate_token(
+        token
+    )
+
+    if not payload:
+
+        return {
+            "error": "Invalid token"
+        }
+
+    return {
+        "message": "Authorized",
+        "user": payload
     }
