1ab6b5e59f926c9731feaf5b25d94e531317dc0b
Bettina Santana de Meirelles <103582720+BettinaSM@users.noreply.github.com>
2026-05-21 04:23:25 -0300

    Update 5_Cloud_Security.py

--- dashboard-app/pages/5_Cloud_Security.py
+++ dashboard-app/pages/5_Cloud_Security.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import pandas as pd
 
+from parsers.session_auth import (
+    require_auth
+)
+
+require_auth()
+
 st.set_page_config(
     page_title="Cloud Security",
     page_icon="☁️",
