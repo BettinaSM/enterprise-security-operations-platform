1228493a2eb54eaae6466da7daf01c2e2453c8a0
Bettina Santana de Meirelles <103582720+BettinaSM@users.noreply.github.com>
2026-05-21 04:00:48 -0300

    Create 6_Live_Monitoring.py

---
+++ dashboard-app/pages/6_Live_Monitoring.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import sqlite3
+import pandas as pd
+
+st.title(
+    "Live Security Monitoring"
+)
+
+connection = sqlite3.connect(
+    "database/security.db"
+)
+
+query = """
+SELECT *
+FROM security_events
+ORDER BY id DESC
+LIMIT 50
+"""
+
+df = pd.read_sql_query(
+    query,
+    connection
+)
+
+st.dataframe(
+    df,
+    use_container_width=True
+)
