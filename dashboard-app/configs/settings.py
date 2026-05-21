5571f479bc987023ff883af8096ddfc9ef179629
Bettina Santana de Meirelles <103582720+BettinaSM@users.noreply.github.com>
2026-05-21 04:46:31 -0300

    Update settings.py

--- dashboard-app/configs/settings.py
+++ dashboard-app/configs/settings.py
@@ -6,6 +6,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SIMULATIONS_DIR = BASE_DIR / "simulations"
 
+THREAT_INTEL_DIR = (
+    BASE_DIR / "threat-intelligence"
+)
+
 RULES_DIR = BASE_DIR / "detections" / "sigma"
 
 THREAT_FEED_DIR = (
