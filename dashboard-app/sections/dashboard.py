0ad6f7802630cc243c08ec2efccf8d54883fa381
Bettina Santana de Meirelles <103582720+BettinaSM@users.noreply.github.com>
2026-05-26 19:25:10 -0300

    Create dashboard.py

---
+++ dashboard-app/sections/dashboard.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+def render_dashboard():
+
+    st.title(
+        "Unified Enterprise Security Operations Platform"
+    )
+
+    st.markdown("""
+Unified Security Operations monitoring platform covering:
+
+- Linux
+- AIX
+- Windows
+- AWS
+- Azure
+- GCP
+- OCI
+- IBM Cloud
+- Kubernetes
+""")
