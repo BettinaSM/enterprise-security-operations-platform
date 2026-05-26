4ed30e5556ac4d6abd668cef5d36eaa0ebdafc1d
Bettina Santana de Meirelles <103582720+BettinaSM@users.noreply.github.com>
2026-05-26 19:09:04 -0300

    Create analytics_service.py

---
+++ dashboard-app/services/analytics_service.py
@@ -0,0 +1,12 @@
+from parsers.analytics_engine import (
+    detection_analytics,
+    incident_analytics
+)
+
+def get_detection_analytics():
+
+    return detection_analytics()
+
+def get_incident_analytics():
+
+    return incident_analytics()
