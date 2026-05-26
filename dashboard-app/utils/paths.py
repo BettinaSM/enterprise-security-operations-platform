5dbd4d3eecab95e5e36d0463021fb09e12895066
Bettina Santana de Meirelles <103582720+BettinaSM@users.noreply.github.com>
2026-05-26 19:20:35 -0300

    Create paths.py

---
+++ dashboard-app/utils/paths.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SIMULATIONS_DIR = (
+    BASE_DIR / "simulations"
+)
+
+THREAT_INTEL_DIR = (
+    BASE_DIR / "threat-intelligence"
+)
