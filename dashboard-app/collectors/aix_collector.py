4b70f068f0f187c744ccd128167e80e75f4808c5
Bettina Santana de Meirelles <103582720+BettinaSM@users.noreply.github.com>
2026-05-27 18:58:00 -0300

    Update aix_collector.py

--- dashboard-app/collectors/aix_collector.py
+++ dashboard-app/collectors/aix_collector.py
@@ -1,29 +1,31 @@
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-AIX_LOG = (
-    BASE_DIR /
-    "agents" /
-    "aix" /
-    "sudo.log"
+from configs.settings import (
+    AIX_SUDO_LOG
 )
+
+# ---------------------------
+# COLLECT AIX LOGS
+# ---------------------------
 
 def collect_aix_logs():
 
-    if not AIX_LOG.exists():
+    try:
 
-        return []
+        with open(
+            AIX_SUDO_LOG,
+            "r",
+            encoding="utf-8"
+        ) as file:
 
-    with open(
-        AIX_LOG,
-        "r"
-    ) as file:
+            logs = file.readlines()
 
-        logs = file.readlines()
+        return [
+            log.strip()
+            for log in logs
+            if log.strip()
+        ]
 
-    return [
-        log.strip()
-        for log in logs
-        if log.strip()
-    ]
+    except Exception as error:
+
+        return [
+            f"aix_collector_error: {error}"
+        ]
