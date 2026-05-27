76bae50b4cf67e21f229328b1ba0ad48859ff32e
Bettina Santana de Meirelles <103582720+BettinaSM@users.noreply.github.com>
2026-05-27 19:53:50 -0300

    Update cloud_collector.py

--- dashboard-app/collectors/cloud_collector.py
+++ dashboard-app/collectors/cloud_collector.py
@@ -1,18 +1,51 @@
-from parsers.cloud_parser import load_json_log
+from configs.settings import (
+    SIMULATION_MODE
+)
+
+from parsers.cloud_parser import (
+    load_json_log
+)
+
+# ---------------------------
+# CLOUD COLLECTOR
+# ---------------------------
 
 def collect_cloud_logs():
 
+    if SIMULATION_MODE:
+
+        return {
+
+            "aws": load_json_log(
+                "agents/aws/cloudtrail.json"
+            ),
+
+            "azure": load_json_log(
+                "agents/azure/entra-signins.json"
+            ),
+
+            "gcp": load_json_log(
+                "agents/gcp/audit-logs.json"
+            ),
+
+            "oci": load_json_log(
+                "agents/oracle-cloud/oci-audit.json"
+            ),
+
+            "ibm": load_json_log(
+                "agents/ibm-cloud/activity-tracker.json"
+            )
+        }
+
+    # ---------------------------
+    # REAL CLOUD COLLECTION
+    # ---------------------------
+
     return {
 
-        "aws": load_json_log(
-            "agents/aws/cloudtrail.json"
-        ),
-
-        "azure": load_json_log(
-            "agents/azure/entra-signins.json"
-        ),
-
-        "gcp": load_json_log(
-            "agents/gcp/audit-logs.json"
-        )
+        "aws": [],
+        "azure": [],
+        "gcp": [],
+        "oci": [],
+        "ibm": []
     }
