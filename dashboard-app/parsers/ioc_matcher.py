792ade507029af7137289539e75c7d2bc0302d07
Bettina Santana de Meirelles <103582720+BettinaSM@users.noreply.github.com>
2026-05-28 14:39:17 -0300

    Update ioc_matcher.py

--- dashboard-app/parsers/ioc_matcher.py
+++ dashboard-app/parsers/ioc_matcher.py
@@ -1,18 +1,57 @@
-known_bad_ips = [
-    "185.220.101.1"
+from parsers.ioc_auto_ingest import (
+    load_remote_iocs
+)
+
+# ---------------------------
+# STATIC IOCS
+# ---------------------------
+
+STATIC_IOCS = [
+
+    "185.220.101.1",
+    "malicious-domain.com"
 ]
 
+# ---------------------------
+# LOAD IOCS
+# ---------------------------
 
-def match_ioc(log_lines):
+def load_all_iocs():
+
+    remote_iocs = load_remote_iocs()
+
+    return list(
+
+        set(
+
+            STATIC_IOCS +
+            remote_iocs
+        )
+    )
+
+# ---------------------------
+# MATCH IOCS
+# ---------------------------
+
+def match_ioc(
+    log_lines
+):
 
     matches = []
 
+    indicators = load_all_iocs()
+
     for line in log_lines:
 
-        for ip in known_bad_ips:
+        for indicator in indicators:
 
-            if ip in line:
+            if indicator in line:
 
-                matches.append(ip)
+                matches.append({
+
+                    "ioc": indicator,
+                    "log": line,
+                    "severity": "Critical"
+                })
 
     return matches
