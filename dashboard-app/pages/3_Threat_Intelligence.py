6e5f2f9e5b56513bd715bb4ec0907b3103014cb2
Bettina Santana de Meirelles <103582720+BettinaSM@users.noreply.github.com>
2026-06-07 18:11:57 -0300

    Update 3_Threat_Intelligence.py

--- dashboard-app/pages/3_Threat_Intelligence.py
+++ dashboard-app/pages/3_Threat_Intelligence.py
@@ -1,3 +1,9 @@
+from parsers.session_auth import (
+    require_auth
+)
+
+require_auth()
+
 import streamlit as st
 import pandas as pd
 import json
@@ -7,8 +13,6 @@ from pathlib import Path
 from parsers.session_auth import (
     require_auth
 )
-
-require_auth()
 
 from configs.settings import (
     THREAT_FEED
