9ff743a885058894bdd3b3983b00bf0a3e372d97
Bettina Santana de Meirelles <103582720+BettinaSM@users.noreply.github.com>
2026-06-08 00:39:52 -0300

    Create identity_review_engine.py

---
+++ dashboard-app/parsers/identity_review_engine.py
@@ -0,0 +1,19 @@
+def create_review_campaign():
+
+    return {
+
+        "campaign":
+
+        "Quarterly Access Review",
+
+        "identities":
+
+        [
+
+            "root",
+            "oracle",
+            "svc_backup",
+            "Administrator"
+
+        ]
+    }
