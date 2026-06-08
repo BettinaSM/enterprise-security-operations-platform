8142bbc2d4233a51ba29d08eff3292a9b73ec8cb
Bettina Santana de Meirelles <103582720+BettinaSM@users.noreply.github.com>
2026-06-08 00:17:02 -0300

    Update windows_audit_engine.py

--- dashboard-app/parsers/windows_audit_engine.py
+++ dashboard-app/parsers/windows_audit_engine.py
@@ -1,1 +1,51 @@
+import json
 
+from configs.settings import (
+    WINDOWS_LOG
+)
+
+# ---------------------------
+# USERS
+# ---------------------------
+
+def get_windows_users():
+
+    return [
+
+        {
+            "username": "Administrator"
+        }
+    ]
+
+# ---------------------------
+# EVENTS
+# ---------------------------
+
+def get_windows_events():
+
+    try:
+
+        with open(
+            WINDOWS_LOG,
+            "r",
+            encoding="utf-8"
+        ) as file:
+
+            return json.load(file)
+
+    except:
+
+        return []
+
+# ---------------------------
+# AUDIT
+# ---------------------------
+
+def run_windows_audit():
+
+    return {
+
+        "users": get_windows_users(),
+        "events": get_windows_events()
+
+    }
