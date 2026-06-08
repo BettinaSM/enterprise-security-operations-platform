1c46424e6feaee76cf78263abd6fabd956576fa0
Bettina Santana de Meirelles <103582720+BettinaSM@users.noreply.github.com>
2026-06-08 01:57:27 -0300

    Create attack_path_engine.py

---
+++ dashboard-app/parsers/attack_path_engine.py
@@ -0,0 +1,19 @@
+def build_attack_paths():
+
+    return [
+
+        {
+
+            "identity":
+                "socuser",
+
+            "path":
+
+                "sudo -> root -> AWS Admin -> K8s Cluster Admin",
+
+            "risk":
+                "Critical"
+
+        }
+
+    ]
