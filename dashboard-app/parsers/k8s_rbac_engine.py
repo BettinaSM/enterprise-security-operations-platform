4f8c7fafadb2ec15d057fcfd2e44796dbd495203
Bettina Santana de Meirelles <103582720+BettinaSM@users.noreply.github.com>
2026-06-20 19:48:46 -0300

    Update k8s_rbac_engine.py

--- dashboard-app/parsers/k8s_rbac_engine.py
+++ dashboard-app/parsers/k8s_rbac_engine.py
@@ -1,15 +1,29 @@
+from kubernetes import client
+
+from integrations.kubernetes_connector import (
+    connect_cluster
+)
+
 def get_k8s_roles():
 
-    return [
+    connect_cluster()
 
-        {
-            "role": "cluster-admin",
-            "namespace": "default"
-        },
+    api = client.RbacAuthorizationV1Api()
 
-        {
-            "role": "view",
-            "namespace": "dev"
-        }
+    roles = api.list_cluster_role()
 
-    ]
+    data = []
+
+    for role in roles.items:
+
+        data.append({
+
+            "role":
+                role.metadata.name,
+
+            "source":
+                "Kubernetes"
+
+        })
+
+    return data
