88d656cfde27c34da895a93cfaff231e56288594
Bettina Santana de Meirelles <103582720+BettinaSM@users.noreply.github.com>
2026-06-20 19:36:15 -0300

    Update ldap_engine.py

--- dashboard-app/parsers/ldap_engine.py
+++ dashboard-app/parsers/ldap_engine.py
@@ -1,15 +1,30 @@
+from integrations.ldap_connector import (
+    ldap_connect
+)
+
 def get_ldap_users():
 
-    return [
+    conn = ldap_connect()
 
-        {
-            "username": "ldap-user01",
+    conn.search(
+
+        "dc=company,dc=com",
+
+        "(objectClass=person)",
+
+        attributes=["cn"]
+
+    )
+
+    users = []
+
+    for entry in conn.entries:
+
+        users.append({
+
+            "username": str(entry.cn),
             "source": "LDAP"
-        },
 
-        {
-            "username": "ldap-admin",
-            "source": "LDAP"
-        }
+        })
 
-    ]
+    return users
