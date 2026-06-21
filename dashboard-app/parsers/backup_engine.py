d678511f963032e625a07a3a9d6c605387b4f5d9
Bettina Santana de Meirelles <103582720+BettinaSM@users.noreply.github.com>
2026-06-21 17:56:31 -0300

    Create backup_engine.py

---
+++ dashboard-app/parsers/backup_engine.py
@@ -0,0 +1,25 @@
+def backup_status():
+
+    return [
+
+        {
+
+            "asset": "srv-linux-01",
+
+            "last_backup": "2026-06-15",
+
+            "status": "Success"
+
+        },
+
+        {
+
+            "asset": "srv-win-01",
+
+            "last_backup": "2026-06-15",
+
+            "status": "Success"
+
+        }
+
+    ]
