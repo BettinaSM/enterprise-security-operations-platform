535f02bca73af9f913bd8ff480b3f90f7276d134
Bettina Santana de Meirelles <103582720+BettinaSM@users.noreply.github.com>
2026-06-21 15:59:31 -0300

    Update logger.py

--- dashboard-app/utils/logger.py
+++ dashboard-app/utils/logger.py
@@ -1,25 +1,72 @@
 import logging
+import sys
 
 from utils.paths import LOG_DIR
 
 LOG_FILE = LOG_DIR / "soc_platform.log"
 
-logging.basicConfig(
+# --------------------------------
+# LOGGER
+# --------------------------------
 
-    filename=LOG_FILE,
-
-    level=logging.INFO,
-
-    format="%(asctime)s | %(levelname)s | %(message)s"
-
+logger = logging.getLogger(
+    "SOC"
 )
 
-logger = logging.getLogger("SOC")
+logger.setLevel(
+    logging.INFO
+)
 
+# evita duplicação
+if not logger.handlers:
+
+    formatter = logging.Formatter(
+
+        "%(asctime)s | %(levelname)s | %(name)s | %(message)s"
+
+    )
+
+    # arquivo
+
+    file_handler = logging.FileHandler(
+        LOG_FILE
+    )
+
+    file_handler.setFormatter(
+        formatter
+    )
+
+    # console
+
+    console_handler = logging.StreamHandler(
+        sys.stdout
+    )
+
+    console_handler.setFormatter(
+        formatter
+    )
+
+    logger.addHandler(
+        file_handler
+    )
+
+    logger.addHandler(
+        console_handler
+    )
+
+
+# --------------------------------
+# HELPERS
+# --------------------------------
 
 def log_info(message):
 
     logger.info(message)
+
+
+def log_warning(message):
+
+    logger.warning(message)
 
 
 def log_error(message):
@@ -27,6 +74,6 @@ def log_error(message):
     logger.error(message)
 
 
-def log_warning(message):
+def log_exception(message):
 
-    logger.warning(message)
+    logger.exception(message)
