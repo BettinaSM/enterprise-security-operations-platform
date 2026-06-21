c3c29c4abf09349b098090036be82892bc07cae6
Bettina Santana de Meirelles <103582720+BettinaSM@users.noreply.github.com>
2026-06-20 23:27:29 -0300

    Create constants.py

---
+++ dashboard-app/core/constants.py
@@ -0,0 +1,36 @@
+SUPPORTED_OS = [
+
+    "Linux",
+    "AIX",
+    "Windows"
+
+]
+
+SUPPORTED_CLOUDS = [
+
+    "AWS",
+    "Azure",
+    "GCP",
+    "OCI",
+    "IBM Cloud"
+
+]
+
+SUPPORTED_COMPLIANCE = [
+
+    "NIST",
+    "ISO27001",
+    "LGPD",
+    "PCI-DSS",
+    "CIS"
+
+]
+
+CRITICALITY_LEVELS = [
+
+    "Low",
+    "Medium",
+    "High",
+    "Critical"
+
+]
