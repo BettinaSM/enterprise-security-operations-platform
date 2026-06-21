bbcc066ffeea8a0fc4826daab701d50a23dad0ba
Bettina Santana de Meirelles <103582720+BettinaSM@users.noreply.github.com>
2026-06-20 23:46:29 -0300

    Create identity.py

---
+++ dashboard-app/domain/identity.py
@@ -0,0 +1,19 @@
+class Identity:
+
+    def __init__(
+
+        self,
+
+        username,
+
+        source,
+
+        privileged=False
+
+    ):
+
+        self.username = username
+
+        self.source = source
+
+        self.privileged = privileged
