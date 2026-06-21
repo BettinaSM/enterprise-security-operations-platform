e3a1ed2872d05d8ca641ba394e2c62cd2821377b
Bettina Santana de Meirelles <103582720+BettinaSM@users.noreply.github.com>
2026-06-21 16:12:06 -0300

    Update __init__.py

--- dashboard-app/api/routers/__init__.py
+++ dashboard-app/api/routers/__init__.py
@@ -1,1 +1,8 @@
-
+from . import auth
+from . import incidents
+from . import detections
+from . import threat_intel
+from . import assets
+from . import risk
+from . import vulnerabilities
+from . import identities
