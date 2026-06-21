623e80eebd2a1930ab2a491ac1a7a1254c4ce572
Bettina Santana de Meirelles <103582720+BettinaSM@users.noreply.github.com>
2026-06-20 21:47:18 -0300

    Update timeline_engine.py

--- dashboard-app/parsers/timeline_engine.py
+++ dashboard-app/parsers/timeline_engine.py
@@ -1,12 +1,10 @@
 from datetime import datetime
 
 # ---------------------------
-# BUILD TIMELINE
+# GENERIC TIMELINE
 # ---------------------------
 
-def build_timeline(
-    events
-):
+def build_timeline(events):
 
     timeline = []
 
@@ -14,8 +12,63 @@ def build_timeline(
 
         timeline.append({
 
-            "timestamp": datetime.utcnow().isoformat(),
-            "event": str(event)
+            "timestamp":
+
+                event.get(
+                    "timestamp",
+                    datetime.utcnow().isoformat()
+                ),
+
+            "event":
+
+                str(event)
+
         })
 
     return timeline
+
+# --------------------------------
+# USER TIMELINE
+# --------------------------------
+
+def get_user_timeline(
+
+    events,
+    username
+
+):
+
+    return [
+
+        event
+
+        for event in events
+
+        if event.get(
+            "username"
+        ) == username
+
+    ]
+
+# --------------------------------
+# HOST TIMELINE
+# --------------------------------
+
+def get_host_timeline(
+
+    events,
+    hostname
+
+):
+
+    return [
+
+        event
+
+        for event in events
+
+        if event.get(
+            "hostname"
+        ) == hostname
+
+    ]
