8aa741070460e79b4696dd47de15b1139af3c904
Bettina Santana de Meirelles <103582720+BettinaSM@users.noreply.github.com>
2026-06-21 00:05:05 -0300

    Create database.py

---
+++ dashboard-app/database/database.py
@@ -0,0 +1,27 @@
+from sqlalchemy import create_engine
+
+from sqlalchemy.orm import sessionmaker
+
+from sqlalchemy.orm import declarative_base
+
+DATABASE_URL = "sqlite:///database/soc.db"
+
+engine = create_engine(
+
+    DATABASE_URL,
+
+    echo=False
+
+)
+
+SessionLocal = sessionmaker(
+
+    autocommit=False,
+
+    autoflush=False,
+
+    bind=engine
+
+)
+
+Base = declarative_base()
