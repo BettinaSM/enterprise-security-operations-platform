280b8c13cb22b32c253279e10fabf353f6aef035
Bettina Santana de Meirelles <103582720+BettinaSM@users.noreply.github.com>
2026-06-20 23:41:47 -0300

    Update paths.py

--- dashboard-app/utils/paths.py
+++ dashboard-app/utils/paths.py
@@ -2,22 +2,53 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SIMULATIONS_DIR = (
-    BASE_DIR / "simulations"
-)
+SIMULATIONS_DIR = BASE_DIR / "simulations"
 
-THREAT_INTEL_DIR = (
-    BASE_DIR / "threat-intelligence"
-)
+THREAT_INTEL_DIR = BASE_DIR / "threat-intelligence"
 
 LOG_DIR = BASE_DIR / "logs"
+
 REPORT_DIR = BASE_DIR / "reports"
+
 BASELINE_DIR = BASE_DIR / "baseline"
+
 INVENTORY_DIR = BASE_DIR / "inventory"
+
 VULN_DIR = BASE_DIR / "vulnerabilities"
+
 CONTROL_DIR = BASE_DIR / "controls"
+
 THREAT_DIR = BASE_DIR / "threat-intelligence"
 
-LOG_DIR.mkdir(exist_ok=True)
-REPORT_DIR.mkdir(exist_ok=True)
-BASELINE_DIR.mkdir(exist_ok=True)
+CMDB_DIR = INVENTORY_DIR
+
+# ---------------------------
+# CREATE DIRECTORIES
+# ---------------------------
+
+for directory in [
+
+    LOG_DIR,
+
+    REPORT_DIR,
+
+    BASELINE_DIR,
+
+    INVENTORY_DIR,
+
+    VULN_DIR,
+
+    CONTROL_DIR,
+
+    THREAT_DIR,
+
+    SIMULATIONS_DIR
+
+]:
+
+    directory.mkdir(
+
+        parents=True,
+
+        exist_ok=True
+    )
