f15e5ad3900d711860158b30fe6ade143e960cac
Bettina Santana de Meirelles <103582720+BettinaSM@users.noreply.github.com>
2026-06-20 23:46:05 -0300

    Create asset.py

---
+++ dashboard-app/domain/asset.py
@@ -0,0 +1,27 @@
+class Asset:
+
+    def __init__(
+
+        self,
+
+        hostname,
+
+        ip,
+
+        os,
+
+        owner,
+
+        criticality
+
+    ):
+
+        self.hostname = hostname
+
+        self.ip = ip
+
+        self.os = os
+
+        self.owner = owner
+
+        self.criticality = criticality
