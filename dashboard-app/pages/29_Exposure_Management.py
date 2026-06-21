7933007402a7beec6b4500e5edde1718d7cb8010
Bettina Santana de Meirelles <103582720+BettinaSM@users.noreply.github.com>
2026-06-21 18:05:58 -0300

    Update 29_Exposure_Management.py

--- dashboard-app/pages/29_Exposure_Management.py
+++ dashboard-app/pages/29_Exposure_Management.py
@@ -1,12 +1,12 @@
 import streamlit as st
 import pandas as pd
 
-from parsers.session_auth import (
-    require_auth
-)
-
 from parsers.ctem_engine import (
     run_ctem_analysis
+)
+
+from parsers.session_auth import (
+    require_auth
 )
 
 require_auth()
