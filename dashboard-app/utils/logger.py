a664f79834784cd9b8913e71689498f8e51bfc2c
Bettina Santana de Meirelles <103582720+BettinaSM@users.noreply.github.com>
2026-06-20 23:29:55 -0300

    Update logger.py

--- dashboard-app/utils/logger.py
+++ dashboard-app/utils/logger.py
@@ -1,9 +1,32 @@
 import logging
 
+from utils.paths import LOG_DIR
+
+LOG_FILE = LOG_DIR / "soc_platform.log"
+
 logging.basicConfig(
-    level=logging.INFO
+
+    filename=LOG_FILE,
+
+    level=logging.INFO,
+
+    format="%(asctime)s | %(levelname)s | %(message)s"
+
 )
 
-logger = logging.getLogger(
-    "enterprise-soc"
-)
+logger = logging.getLogger("SOC")
+
+
+def log_info(message):
+
+    logger.info(message)
+
+
+def log_error(message):
+
+    logger.error(message)
+
+
+def log_warning(message):
+
+    logger.warning(message)
